8a0ad65f9f0c40bb76682e68dbf7d71bdba1148a
GeorgeFu77 <georgefu777@gmail.com>
2026-08-15 15:05:55 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ words=open("words.csv")
 reader=csv.DictReader(words)
 word=list(reader) 
 print(len(word))
+
+choice = input("1) View all words, 2) Search, 3) Exit")
+if(choice == "1"):
+  print("words")
