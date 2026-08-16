ca284081bacf6a56d5d72269a674ad2f33f18f7f
GeorgeFu77 <georgefu777@gmail.com>
2026-08-16 11:27:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
-import csv 
-print("diggity dawg")
-words=open("words.csv") 
-reader=csv.DictReader(words)
-word=list(reader) 
-print(len(word))
+import csv
 
-choice = input("1) View all words, 2) Search, 3) Exit")
-if(choice == "1"):
-  for row in word:
-    print(row["word"],row["definition"],row["set"])
-elif(choice == "2"):
-  print("search")
-elif(choice == "3"): 
-  quit()
-else:
-  print("you are actually stupid")
+print("diggity dawg")
+
+words = open("words.csv")
+reader = csv.DictReader(words)
+word = list(reader)
+
+print(len(word))
+while True:  
+  choice = input("1) View all words, 2) Search, 3) Exit: ")
+  
+  if choice == "1":
+      for row in word:
+          print(row["word"], row["definition"], row["set"])
+  
+  elif choice == "2":
+      search = input("What word do you want to search? ").lower().strip()
+  
+      found = False
+  
+      for row in word:
+          if row["word"].lower() == search:
+              print("Word:", row["word"])
+              print("Definition:", row["definition"])
+              print("Set:", row["set"])
+              found = True
+  
+      if found == False:
+          print("Word not found")
+  
+  elif choice == "3":
+      quit()
+  
+  else:
+      print("you are actually stupid")
